2c39f1d3f943a4e100dd8fd9fa2fcb8c20b5e987
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-16 18:17:06 +0100

    Contact.java and ContactsManager.java added

---
+++ src/com/udacity/contactsmanager/Contact.java
@@ -0,0 +1,7 @@
+package com.udacity.contactsmanager;
+
+public class Contact {
+    String name;
+    String email;
+    String phoneNumber;
+}
---
+++ src/com/udacity/contactsmanager/ContactsManager.java
@@ -0,0 +1,25 @@
+package com.udacity.contactsmanager;
+
+class ContactsManager {
+    Contact[] myFriends;
+    int friendsCount;
+
+    ContactsManager() {
+        this.myFriends = new Contact[500];
+        this.friendsCount = 0;
+    }
+
+    void addContact(Contact contact) {
+        myFriends[friendsCount] = contact;
+        friendsCount++;
+    }
+
+    Contact searchContact(String searchName) {
+        for (int i = 0; i < friendsCount; i++) {
+            if (myFriends[i].name.equals(searchName)) {
+                return myFriends[i];
+            }
+        }
+        return null;
+    }
+}
