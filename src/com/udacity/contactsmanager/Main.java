0a05c7bb51e4ba1675702a2f5f4de7fac5ae020a
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-16 18:23:14 +0100

    main method added in Main.java

---
+++ src/com/udacity/contactsmanager/Main.java
@@ -0,0 +1,7 @@
+package com.udacity.contactsmanager;
+
+public class Main {
+    public static void main(String[] args) {
+        ContactsManager myContactsManager = new ContactsManager();
+    }
+}
