39f91fada0b9837cf3599ca59e376123e3f6bba3
Lukasz Spyra <lukasz.spyrka@gmail.com>
2020-02-19 18:51:01 +0100

    javadocs comments added to GuessTheMovie.java

--- src/com/udacity/guessthemovie/GuessTheMovie.java
+++ src/com/udacity/guessthemovie/GuessTheMovie.java
@@ -2,31 +2,46 @@ package com.udacity.guessthemovie;
 
 import java.util.Arrays;
 
+/**
+ * Guess the Movie prepares and starts the actual game and checks letters being guessed by the Player.
+ * Displays: Game Over! or You Win.
+ */
 public class GuessTheMovie {
     Game game;
     int mistakeCounter;
 
+    /**
+     * Constructor sets the mistake counter to 0
+     */
     public GuessTheMovie() {
         game = new Game();
         mistakeCounter = 0;
     }
 
+    /**
+     * prepareGame call methods of Game class to read movies from file and pick random title in order to start game.
+     */
     public void prepareGame() {
         game.readMovieFile("movies.txt");
         game.pickRandomTitle();
-        game.convertLettersToUnderscores();
     }
 
+    /**
+     * gameStart displays messages to Player with short description and number of letter of the movie to be figured out.
+     */
     public void gameStart() {
         System.out.println("Hi! Try to figure out the movie by guessing one letter at a time!");
         System.out.println("Your mysterious title contains " + game.getTitle().length() + " letters,");
     }
 
+    /** gamePlay is performing the game process, checking the letters typed by the Player, deciding about the game result.
+     *
+     * @return message if Player have won or lost as String.
+     */
     public String gamePlay() {
         String gameResult = "Game Over. Try again.";
-        String underscoreTitle = game.convertLettersToUnderscores();
         char[] arrayTitle = game.getTitle().toCharArray();
-        char[] arrayUnderscoreTitle = game.convertLettersToUnderscores().toCharArray();
+        char[] arrayUnderscoreTitle = game.convertLettersToUnderscores(game.getTitle()).toCharArray();
         StringBuilder wrongLetters = new StringBuilder();
         while (mistakeCounter <= 10) {
             System.out.println("You are guessing: " + String.valueOf(arrayUnderscoreTitle));
@@ -34,7 +49,7 @@ public class GuessTheMovie {
             System.out.print("Guess a letter: ");
             char letterGuess = game.getUserInput();
             if (game.getTitle().indexOf(letterGuess) == -1) {
-                if(wrongLetters.indexOf(String.valueOf(letterGuess)) == -1) {
+                if (wrongLetters.indexOf(String.valueOf(letterGuess)) == -1) {
                     wrongLetters.append(letterGuess);
                     mistakeCounter++;
                 }
@@ -51,7 +66,7 @@ public class GuessTheMovie {
                 break;
             }
         }
-     return gameResult;
+        return gameResult;
     }
 
     public static void main(String[] args) {
